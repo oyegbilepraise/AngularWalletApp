4d970d59f915780c535507f563063fa46d01051d
Praise <oluwadhammieh@gmail.com>
2021-07-19 01:04:40 +0100

    updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -666,9 +666,23 @@ export class DashboardComponent implements OnInit {
   }
   goToP(url: any, userId: any, cmp: any) {
     this._test.show.next(false)
+    let a: any = document.getElementsByClassName('navbar')[0]
+    if (a.style.display === 'block') {
+      a.style.display = 'none'
+    }
+    else {
+      a.style.display = 'block'
+    }
   }
   goToD(url: any, userId: any, cmp: any) {
     this._test.show.next(true)
+    let a: any = document.getElementsByClassName('navbar')[0]
+    if (a.style.display === 'block') {
+      a.style.display = 'none'
+    }
+    else {
+      a.style.display = 'block'
+    }
   }
   showPhoneDash() {
     let a: any = document.getElementsByClassName('navbar')[0]
